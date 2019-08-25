4ad1c4967b6a46107f300afbcc9aacbf62e67d48
Christos Christou <christos.n.christou@gmail.com>
2019-08-25 12:46:47 +0300

    Add files via upload

---
+++ devtools.js
@@ -0,0 +1,5 @@
+//Console textarea fix
+var textarea = document.querySelector(".eruda-dev-tools .eruda-tools .eruda-console .eruda-js-input textarea");
+textarea.autocorrect = "off";
+textarea.autocapitalize = "off";
+textarea.spellcheck = "off";
