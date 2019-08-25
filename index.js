8340db56837c0c038c4b04f2fbcb9ed5bf342836
Christos Christou <christos.n.christou@gmail.com>
2019-08-25 13:53:18 +0300

    last commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.async = true;
+script.src = 'https://cdn.jsdelivr.net/npm/eruda';
+script.onload = function(){
+  var loaded = document.createElement('script');
+  loaded.type = 'text/javascript';
+  loaded.innerHTML = "eruda.init(); var stylesheet = document.createElement('style'); var scriptFix = document.createElement('script'); fetch('https://raw.githubusercontent.com/christosnc/ios-devtools/master/devtools.css').then(response => response.text()).then((data) => {stylesheet.innerHTML = data; eruda._shadowRoot.appendChild(stylesheet);}); fetch('https://raw.githubusercontent.com/christosnc/ios-devtools/master/devtools.js').then(response => response.text()).then((data) => {scriptFix.innerHTML = data; document.body.appendChild(scriptFix); erudaFix(eruda._shadowRoot);});";
+  document.body.appendChild(loaded);
+}
+document.body.appendChild(script);
