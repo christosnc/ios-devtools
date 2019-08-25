ee61950778fb0691db1ba2077a8e29813efc9f73
Christos Christou <christos.n.christou@gmail.com>
2019-08-25 12:50:51 +0300

    Update devtools.js

--- devtools.js
+++ devtools.js
@@ -1,5 +1,7 @@
-//Console textarea fix
-var textarea = document.querySelector(".eruda-dev-tools .eruda-tools .eruda-console .eruda-js-input textarea");
-textarea.autocorrect = "off";
-textarea.autocapitalize = "off";
-textarea.spellcheck = "off";
+function erudaFix(root){
+  //Console textarea fix
+  var textarea = root.querySelector(".eruda-dev-tools .eruda-tools .eruda-console .eruda-js-input textarea");
+  textarea.autocorrect = "off";
+  textarea.autocapitalize = "off";
+  textarea.spellcheck = "off";
+}
